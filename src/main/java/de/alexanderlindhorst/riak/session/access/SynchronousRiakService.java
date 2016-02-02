4dc7c928bf47c2fc4d8839c5a692c95816125290
Alexander Lindhorst <alindhorst@ebay-kleinanzeigen.de>
2016-02-02 08:34:40 +0100

    Synchronous implementation for Riak access started

--- src/main/java/de/alexanderlindhorst/riak/session/access/SynchronousRiakService.java
+++ src/main/java/de/alexanderlindhorst/riak/session/access/SynchronousRiakService.java
@@ -3,8 +3,62 @@
  */
 package de.alexanderlindhorst.riak.session.access;
 
+import java.net.UnknownHostException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.basho.riak.client.core.RiakNode;
+
+import de.alexanderlindhorst.riak.session.manager.RiakSession;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
+
 /**
  * @author alindhorst
  */
-public abstract class SynchronousRiakService extends RiakServiceBase {
+public class SynchronousRiakService extends RiakServiceBase {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SynchronousRiakService.class);
+    private RiakNode node;
+
+    @Override
+    protected void persistSessionInternal(String sessionId, RiakSession session) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected RiakSession getSessionInternal(String sessionId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void deleteSessionInternal(String sessionId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void init() {
+        Pattern addressPattern = Pattern.compile("^(?<host>[^:]+)(:(?<port>.+))?");
+        Matcher matcher = addressPattern.matcher(getBackendAddress());
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("backend address value " + getBackendAddress() + " cannot be read");
+        }
+        String host = matcher.group("host");
+        String portValue = matcher.group("port");
+        int port = 10017;
+        if (!isNullOrEmpty(portValue)) {
+            port = Integer.valueOf(portValue);
+        }
+        try {
+            node = new RiakNode.Builder()
+                    .withRemoteAddress(host)
+                    .withRemotePort(port)
+                    .build();
+        } catch (UnknownHostException ex) {
+            throw new IllegalStateException("Couldn't configure riak access", ex);
+        }
+    }
 }
